39041ce644c0623a6f7f3b15560ed7400214e620
RJ Graffham <psquid@psquid.net>
2026-08-13 16:31:06 +0100

    Add Debian 12 cloud base image as volume

--- main.tf
+++ main.tf
@@ -1,16 +1,32 @@
 provider "libvirt" {}
 
-resource "libvirt_network" "cluster-net" {
+resource "libvirt_volume" "debian_cloud_base" {
+  name = "debian-12-generic-amd64.qcow2"
+  pool = "default"
+  target = {
+    format = {
+      type = "qcow2"
+    }
+  }
+
+  create = {
+    content = {
+      url = "https://cloud.debian.org/images/cloud/bookworm/latest/debian-12-generic-amd64.qcow2"
+    }
+  }
 }
 
-resource "libvirt_domain" "jumpbox" {
-}
-
-resource "libvirt_domain" "server" {
-}
-
-resource "libvirt_domain" "node-0" {
-}
-
-resource "libvirt_domain" "node-1" {
-}
+#resource "libvirt_network" "cluster_net" {
+#}
+#
+#resource "libvirt_domain" "jumpbox" {
+#}
+#
+#resource "libvirt_domain" "server" {
+#}
+#
+#resource "libvirt_domain" "node-0" {
+#}
+#
+#resource "libvirt_domain" "node-1" {
+#}
